80b28e57f3b1a73bee9b0af027a5380022b17932
Ryo <ryoctrl@gmail.com>
2017-12-24 04:46:51 +0900

    Initial Commit

---
+++ TradeSupporter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TradeSupporter
+//
+//  Created by mosin on 2017/12/24.
+//  Copyright © 2017年 mosin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
